c057dcd7565ce60b5ec5417e1654c81bf5e2628c
Kyle Mitchell <kyle@kemitchell.com>
2015-02-17 02:47:01 +0000

    inline typing hints

--- source/js/components/typing-guide.js
+++ source/js/components/typing-guide.js
@@ -18,26 +18,28 @@ module.exports = React.createClass({
         key: 'body',
         className: 'panel-body'
       },
-        DOM.p({key: 'definition'}, [
+        DOM.ul({key: 'list', className: 'list-inline'}, [
+        DOM.li({key: 'definition'}, [
           span({key: 'lead'}, 'Define terms with double quotes, like '),
           kbd({key: 'keys'}, '""Agreement""'),
           span({key: 'period'}, '.')
         ]),
-        DOM.p({key: 'use'},
+        DOM.li({key: 'use'},
           span({key: 'lead'}, 'Use defined terms with chevrons, like '),
           kbd({key: 'keys'}, '<Consideration>'),
           span({key: 'period'}, '.')
         ),
-        DOM.p({key: 'field'},
+        DOM.li({key: 'field'},
           span({key: 'lead'}, 'Insert blanks with brackets, like '),
           kbd({key: 'keys'}, ['[Company Name]']),
           span({key: 'period'}, '.')
         ),
-        DOM.p({key: 'reference'},
+        DOM.li({key: 'reference'},
           span({key: 'lead'}, 'Reference forms with braces, like '),
           kbd({key: 'keys'}, ['{Indemnification}']),
           span({key: 'period'}, '.')
         )
+        ])
       )
     ]);
   }
