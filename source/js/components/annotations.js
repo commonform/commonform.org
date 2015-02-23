eafc395c9a712f8439f955243bebad73567e277e
Kyle Mitchell <kyle@kemitchell.com>
2015-02-23 18:34:34 +0000

    line breaks in digests

--- source/js/components/annotations.js
+++ source/js/components/annotations.js
@@ -1,17 +1,21 @@
 var ImmutableMixin = require('react-immutable-render-mixin');
 var React = require('react');
 
+var DigestLine = require('./digest-line');
+
 module.exports = React.createClass({
   displayName: 'Annotations',
 
   mixins: [ImmutableMixin],
 
   render: function() {
-    return React.DOM.span({}, [
-      React.DOM.p({
-        className: 'digest',
-        key: 'digest'
-      }, this.props.digest)
+    return React.DOM.div({
+      className: 'annotations',
+    }, [
+      React.createElement(DigestLine, {
+        key: 'digest',
+        digest: this.props.digest
+      })
     ]);
   }
 });
--- source/js/components/digest-line.js
+++ source/js/components/digest-line.js
@@ -6,10 +6,14 @@ module.exports = React.createClass({
   displayName: 'DigestLine',
 
   render: function() {
-    return DOM.div({className: 'row'},
-      DOM.div({className: 'col-sm-12 digest'},
-        'Fingerprint: ' + this.props.digest
-      )
+    return DOM.p({className: 'digest'},
+      this.props.digest.slice(0, 16),
+      React.createElement('wbr', {key: 'split1'}),
+      this.props.digest.slice(16, 32),
+      React.createElement('wbr', {key: 'split2'}),
+      this.props.digest.slice(32, 48),
+      React.createElement('wbr', {key: 'split3'}),
+      this.props.digest.slice(48)
     );
   }
 });
