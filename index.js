e719a9d770cd5b91044cfaf564c372cea54807d4
Kyle Mitchell <kyle@kemitchell.com>
2015-08-16 18:55:42 -0700

    Consolidate entry point logic

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 Function.prototype.bind = (
   Function.prototype.bind || require('function-bind') )
 
-var loop = require('./loop')
+var analyze = require('commonform-analyze')
+var lint = require('commonform-lint')
+
+var loop
+
+function deriveData(state) {
+  state.annotations = lint(state.data)
+  state.analysis = analyze(state.data) }
+
+var state = {
+  path: [ ],
+  blanks: { },
+  title: 'Agreement',
+  update: function(mutation) {
+    mutation(state)
+    deriveData(state)
+    loop.update(state) },
+  data: require('./initial-data.json') }
+
+deriveData(state)
+
+loop = require('main-loop')(
+  state, require('./renderers'), require('virtual-dom'))
 
 document
   .querySelector('#browser')
