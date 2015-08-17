a4373013c7f8e181e3eead5b192567d30baa2379
Kyle Mitchell <kyle@kemitchell.com>
2015-08-16 18:48:49 -0700

    Add JSON save

--- renderers/main-menu.js
+++ renderers/main-menu.js
@@ -2,6 +2,15 @@ var h = require('virtual-dom/h')
 var docx = require('commonform-docx')
 var filesaver = require('filesaver.js').saveAs
 
+function jsonBlob(object) {
+  return new Blob(
+    [ JSON.stringify(object) ],
+    { type: 'application/json' }) }
+
+function fileName(title, extension) {
+  var date = new Date().toISOString();
+  return ( '' + title + ' ' + date + '.' + extension ) }
+
 function mainMenu(state) {
   return h('div.mainMenu', [
     h('a.saveDOCX',
@@ -12,10 +21,24 @@ function mainMenu(state) {
           var form = state.data
           var blanks = state.blanks
           var zip = docx(title, form, blanks)
-          var date = new Date().toISOString();
+          var date = new Date().toISOString()
           filesaver(
-            zip.generate({type: 'blob'}),
-            title + ' ' + date + '.docx') } },
-      'Save DOCX') ]) }
+            zip.generate({ type: 'blob' }),
+            filename(title, 'docx')) } },
+      'Save DOCX'),
+    h('a.saveJSON',
+      { href: '#',
+        onclick: function(event) {
+          event.preventDefault()
+          var title = state.title
+          var date = new Date().toISOString()
+          filesaver(
+            jsonBlob({
+              path: [ ],
+              title: state.title,
+              blanks: state.blanks,
+              data: state.data }),
+            fileName(title, 'json')) } },
+      'Save JSON') ]) }
 
 module.exports = mainMenu
