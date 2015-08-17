118cd58f4e299aefb5103a169905260c53970607
Kyle Mitchell <kyle@kemitchell.com>
2015-08-16 20:59:20 -0700

    Make annotation source names hyperlinks

--- renderers/annotation.js
+++ renderers/annotation.js
@@ -1,11 +1,18 @@
 var h = require('virtual-dom/h')
 
+var SOURCE_URLS = {
+  'commonform-critique': 'https://npmjs.com/packages/commonform-critique',
+  'commonform-lint': 'https://npmjs.com/packages/commonform-lint' }
+
 function annotation(state) {
   var data = state.data
+  var source = data.source
   return h('div.annotation', [
     ( data.url ?
       h('a', { href: data.url }, data.message) :
       data.message ),
-    ' (' + data.source + ')' ]) }
+    ' (',
+    h('a', { href: SOURCE_URLS[source], target: '_blank' }, source),
+    ')' ]) }
 
 module.exports = annotation
