cbd03aeecd2f846ccec3682bf33a82bec6ef4874
Kyle Mitchell <kyle@kemitchell.com>
2015-08-20 17:47:08 -0700

    Simplify annotation path matching

--- renderers/annotations.js
+++ renderers/annotations.js
@@ -6,10 +6,7 @@ function annotations(state) {
   var path = state.path.concat('content')
   var matchingAnnotations = state.annotations
     .filter(function(annotation) {
-      var annotationPath = annotation.path
-      return deepEqual(
-        annotationPath.slice(0, annotationPath.length - 1),
-        path) })
+      return deepEqual(annotation.path.slice(0, -1), path) })
   if (matchingAnnotations.length > 0) {
     return h('div.annotations',
       matchingAnnotations
