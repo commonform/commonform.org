16e223468af970a7d256bf6df87c417a36af1542
Kyle Mitchell <kyle@kemitchell.com>
2015-08-20 22:07:40 -0700

    Load from api.commonform.org

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ Function.prototype.bind = (
 var analyze = require('commonform-analyze')
 var critique = require('commonform-critique')
 var lint = require('commonform-lint')
+var downloadForm = require('./download-form')
 
 var bus = new (require('events').EventEmitter)
 
@@ -16,17 +17,24 @@ var state = {
   blanks: { },
   title: defaultTitle,
   emit: bus.emit.bind(bus),
-  data: require('./initial-data.json') }
+  data: { content: [ 'No content loaded' ] } }
 
-state.errors = lint(state.data)
-state.critiques = critique(state.data)
+function compute() {
+  state.errors = lint(state.data)
+  state.critiques = critique(state.data)
 
-state.annotations = state.errors
-  .concat(state.critiques)
+  state.annotations = state.errors
+    .concat(state.critiques)
 
-state.analysis = analyze(state.data)
+  state.analysis = analyze(state.data) }
+
+compute()
 
 bus
+  .on('form', function(form) {
+    state.data = form
+    compute()
+    loop.update(state) })
   .on('blank', function(blank, value) {
     if (!value || value.length === 0) {
       delete state.blanks[blank] }
@@ -48,3 +56,11 @@ loop = require('main-loop')(
 document
   .querySelector('#browser')
   .appendChild(loop.target)
+
+var initialDigest = 'b8db30d573fbf7ac65c40960cb6301b58087694b0fcc7663897834c22a861dfc'
+
+downloadForm(initialDigest, function(error, response) {
+  if (error) {
+    console.error(error) }
+  else {
+    bus.emit('form', response.form) } })
