ecf236d362cc167a78615cc424cee4645a5abb68
Kyle Mitchell <kyle@kemitchell.com>
2015-08-21 17:43:56 -0700

    Fix blanks table display

--- renderers/blanks.js
+++ renderers/blanks.js
@@ -3,17 +3,17 @@ var blankEntryRow = require('./blank-entry-row')
 
 function blanks(state) {
   return h('div.blanks',
-    h('table.blanks',
-      [ h('thead', [
-          h('th', 'Blank'),
-          h('th', 'Value') ]),
+    h('table',
+      [ h('thead',
+          [ h('th', 'Blank'),
+            h('th', 'Value') ]),
         h('tbody',
-         Object.keys(state.analysis)
-           .map(function(blank) {
-             return blankEntryRow({
-               digest: state.digest,
-               blank: blank,
-               emit: state.emit,
-               values: state.values }) })) ])) }
+          Object.keys(state.analysis)
+            .map(function(blank) {
+              return blankEntryRow({
+                digest: state.digest,
+                blank: blank,
+                emit: state.emit,
+                values: state.values }) })) ])) }
 
 module.exports = blanks
