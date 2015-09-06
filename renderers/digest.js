fce081ccc8079e1c8f5cd512ef55f01b3d52b2e3
Kyle Mitchell <kyle@kemitchell.com>
2015-09-05 21:47:28 -0700

    Allow digests to break into lines

--- renderers/digest.js
+++ renderers/digest.js
@@ -1,9 +1,16 @@
 var h = require('virtual-dom/h')
 
 function fingerprintLink(state) {
+  var digest = state.digest
   return h('a.digest',
-    { href: ( '/forms/' + state.digest ),
+    { href: ( '/forms/' + digest ),
       target: '_blank' },
-    state.digest) }
+    [ digest.slice(0, 16),
+      h('wbr'),
+      digest.slice(16, 32),
+      h('wbr'),
+      digest.slice(32, 48),
+      h('wbr'),
+      digest.slice(48) ]) }
 
 module.exports = fingerprintLink
