8615934e918967787e0bf042a501e83a6736c104
Kyle Mitchell <kyle@kemitchell.com>
2015-09-05 22:01:29 -0700

    Resize <textarea>s on value change

--- renderers/textarea.js
+++ renderers/textarea.js
@@ -1,7 +1,17 @@
 var h  = require('virtual-dom/h')
+var markup = require('commonform-markup')
+var resizeTextarea = require('../resize-textarea')
+
+function resizeTarget(event) {
+  // Delay a tiny amount so the new character, if any, will be added to the
+  // <texarea> value by the time the resize routine runs.
+  setTimeout(function() { resizeTextarea(event.target) }, 1) }
 
 function textarea(state) {
   return h('textarea',
-    { value: state.value }) }
+    { value: markup.stringify(state.data),
+      onkeydown: resizeTarget,
+      onpaste: resizeTarget,
+      onchange: resizeTarget }) }
 
 module.exports = textarea
