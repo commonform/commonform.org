0e8fef605183dd5b89f330c7edb08086adb9b5ae
Kyle Mitchell <kyle@kemitchell.com>
2015-09-05 19:20:08 -0700

    Remove annotation source links

--- renderers/annotation.js
+++ renderers/annotation.js
@@ -1,22 +1,10 @@
 var h = require('virtual-dom/h')
 
-var URL_PREFIX = 'https://npmjs.com/packages/commonform-'
-var SOURCE_URLS = [ 'archaic', 'critique', 'lint' ]
-  .reduce(
-    function(urls, module) {
-      urls['commonform-' + module] = URL_PREFIX + module
-      return urls },
-    { })
-
 function annotation(state) {
   var data = state.data
-  var source = data.source
   return h(( 'p.' + data.level ), [
     ( data.url ?
       h('a', { href: data.url }, data.message) :
-      data.message ),
-    ' (',
-    h('a', { href: SOURCE_URLS[source], target: '_blank' }, source),
-    ')' ]) }
+      data.message ) ]) }
 
 module.exports = annotation
