0fa560ba8ab020a23542685143637ab570f8acbc
Kyle Mitchell <kyle@kemitchell.com>
2016-01-17 01:57:15 -0800

    Configure Travis CI to test with Sauce Labs

--- test/webdriver.js
+++ test/webdriver.js
@@ -1,8 +1,22 @@
 module.exports = function() {
   return require('webdriverio')
-    .remote({
-      host: 'localhost',
-      port: 9515,
-      desiredCapabilities: { browserName: 'chrome' } })
+    .remote(configuration())
     .init()
     .url('http://localhost:8000') }
+
+function configuration() {
+  return (
+    process.env.CI === 'true' ?
+      { host: 'localhost',
+        port: 4445,
+        user: process.env.SAUCE_USERNAME,
+        key: process.env.SAUCE_ACCESS_KEY,
+        desiredCapabilities: {
+          browserName: 'chrome',
+          platform: 'Windows 7',
+          version: '47.0',
+          'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER } } :
+      { host: 'localhost',
+        port: 9515,
+        desiredCapabilities: {
+          browserName: 'chrome' } } ) }
