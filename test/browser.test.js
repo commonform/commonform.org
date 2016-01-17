b13912e8be81ebdc0814854e7772917a212ae70c
Kyle Mitchell <kyle@kemitchell.com>
2016-01-17 11:48:05 -0800

    Reuse one Webdriver client for all browser tests

---
+++ test/browser.test.js
@@ -0,0 +1,28 @@
+var tape = require('tape')
+
+var webdriver = require('./webdriver')()
+
+tape('Browser', function(test) {
+  test.test('Sanity Check', function(test) {
+    test.plan(1)
+    webdriver
+      .getText('a.openSource')
+      .then(function(value) {
+        test.equal(
+        value,
+        'Common Form is open-source software.',
+        'a.openSource says "Common Form is open-source software."') }) })
+
+  test.test('Welcome', function(test) {
+    test.plan(1)
+    webdriver
+      .url('http://localhost:8000')
+      .waitForExist('.heading=Welcome')
+      .isExisting('.heading=Welcome')
+      .then(function(existing) {
+        test.assert(
+          existing,
+          'Page displays "Welcome" heading.') }) }) })
+
+tape.onFinish(function() {
+  webdriver.end() })
