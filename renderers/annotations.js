9d2d5534f3647401f6d70102d93699dddc7780a1
Kyle Mitchell <kyle@kemitchell.com>
2016-01-19 19:24:01 -0800

    Deduplicate annotations

--- renderers/annotations.js
+++ renderers/annotations.js
@@ -3,11 +3,26 @@ module.exports = annotations
 var h = require('virtual-dom/h')
 
 function annotations(annotations) {
-  return h('aside', annotations.map(annotation)) }
+  return h('aside', deduplicate(annotations).map(annotation)) }
 
 function annotation(annotation) {
   return h('p.',
     { className: annotation.level },
     [ ( annotation.url ?
-          h('a', { href: annotation.url }, annotation.message) :
+          ('a', { href: annotation.url }, annotation.message) :
           annotation.message ) ]) }
+
+function deduplicate(annotations) {
+  return annotations
+    .reduce(
+      function(annotations, element) {
+        return (
+          annotations.some(
+            function(otherElement) {
+              return (
+                ( element.source === otherElement.source ) &&
+                ( element.message === otherElement.message ) &&
+                ( element.url === otherElement.url ) ) }) ?
+            annotations :
+            annotations.concat(element) ) },
+      [ ]) }
