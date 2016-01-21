4090f14bf791c242e5bc50241b64489d4ec2c78a
Kyle Mitchell <kyle@kemitchell.com>
2016-01-21 13:15:10 -0800

    Credit Mathew Butterick in footer

--- renderers/footer.js
+++ renderers/footer.js
@@ -4,7 +4,21 @@ var h = require('virtual-dom/h')
 
 function footer() {
   return h('footer',
-    h('a.openSource',
-      { href: 'https://github.com/commonform',
-        target: '_blank' },
-      'Common Form is open-source software.')) }
+    [ h('p',
+        [ h('a',
+            { href: 'http://typographyforlawyers.com/equity.html' },
+            'Equity'),
+          ' and ',
+          h('a',
+            { href: 'http://practicaltypography.com/triplicate.html' },
+            'Triplicate'),
+          ' typefaces by ',
+          h('a',
+            { href: 'http://typographyforlawyers.com/about.html' },
+            'Matthew Butterick'),
+          '.' ]),
+      h('p',
+        [ h('a.openSource',
+          { href: 'https://github.com/commonform',
+            target: '_blank' },
+          'Common Form is open-source software.') ]) ]) }
