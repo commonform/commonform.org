105899f735d7f586e0e9a9995b2318ac6f6ada0c
Kyle Mitchell <kyle@kemitchell.com>
2016-01-22 00:44:14 -0800

    Test appearance of "Signature Page[s] Follow[s]"

--- test/browser.test.js
+++ test/browser.test.js
@@ -51,17 +51,53 @@ tape('Browser', function(test) {
           'Page displays form from API.') }) })
 
   test.test('Signature Page Button', function(test) {
-    test.plan(1)
-    var addButton = '//button[contains(text(),"Add Signature Page")]'
-    webdriver
-      .url('http://localhost:8000')
-      .waitForExist(addButton)
-      .click(addButton)
-      .isExisting('//*[contains(text(),"Signature Page Follows")]')
-      .then(function(existing) {
-        test.assert(
-          existing,
-          'Clicking "Add Signature Page" adds signature pages.') }) }) })
+    var addButton =    '//button[contains(text(),"Add Signature Page")]'
+    var deleteButton = '//button[contains(text(),"Delete this Signature Page")]'
+    var pageFollows = '//*[contains(text(),"Signature Page Follows")]'
+    var pagesFollow = '//*[contains(text(),"Signature Pages Follow")]'
+
+    test.test(function(test) {
+      test.plan(1)
+      webdriver
+        .url('http://localhost:8000')
+        .waitForExist(addButton)
+        .click(addButton)
+        .isExisting(pageFollows)
+        .then(function(existing) {
+          test.assert(
+            existing,
+            'On clicking "Add Signature Page", ' +
+            'the text "Signature Page Follows" appears.') }) })
+
+    test.test(function(test) {
+      test.plan(1)
+      webdriver
+        .url('http://localhost:8000')
+        .waitForExist(addButton)
+        .click(addButton)
+        .click(addButton)
+        .isExisting(pagesFollow)
+        .then(function(existing) {
+          test.assert(
+            existing,
+            'On clicking "Add Signature Page" twice, ' +
+            'the text "Signature Pages Follow" appears.') }) })
+
+    test.test(function(test) {
+      test.plan(1)
+      webdriver
+        .url('http://localhost:8000')
+        .waitForExist(addButton)
+        .click(addButton)
+        .waitForExist(deleteButton)
+        .click(deleteButton)
+        .isExisting(pageFollows)
+        .then(function(existing) {
+          test.assert(
+            !existing,
+            'On clicking "Add Signature Page" ' +
+            'and then "Delete this Signature Page", '+
+            'the text "Signature Page Follows" disappears.') }) }) }) })
 
 tape.onFinish(function() {
   webdriver.end() })
