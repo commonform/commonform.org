8550c45b22d27ea971766ad5bd5f083a3752387d
Kyle E. Mitchell <kyle@kemitchell.com>
2016-05-08 21:29:59 -0700

    Display insertions and deletions of entire children

--- renderers/diff.js
+++ renderers/diff.js
@@ -16,24 +16,34 @@ function renderDiff(state) {
   var root = !diff.hasOwnProperty('form')
   var formLike = ( root ? diff : diff.form )
   var groups = group(jsonClone(formLike))
+  var wrapper
+  if (diff.hasOwnProperty('inserted')) {
+    wrapper = function wrapInIns(argument) {
+      return h('ins', argument ) } }
+  else if (diff.hasOwnProperty('deleted')) {
+    wrapper = function wrapInDel(argument) {
+      return h('del', argument ) } }
+  else {
+    wrapper = function doNotWrap(argument) {
+      return argument } }
 
   // Rendering
-  return [
-    h('section',
-      // TODO: Show conspicuous changes
-      { className: classnames({
-          conspicuous: formLike.conspicuous.some(function(element) {
-            return ( !element.hasOwnProperty('deleted') ) }) }) },
-      [ ( root
-            ? null
-            : h('a.sigil', '§') ),
-        ( Array.isArray(diff.heading)
-            ? renderDiffHeading({ heading: diff.heading })
-            : null ),
-        groups
-          .map(function(group) {
-            var renderer = (
-              ( group.type === 'series' )
-                ? renderDiffSeries
-                : renderDiffParagraph )
-            return renderer({ data: group }) }) ]) ] }
+  return h('section',
+    // TODO: Show conspicuous changes
+    { className: classnames({
+        conspicuous: formLike.conspicuous.some(function(element) {
+          return ( !element.hasOwnProperty('deleted') ) }) }) },
+    wrapper([
+      ( root
+          ? null
+          : h('a.sigil', '§') ),
+      ( Array.isArray(diff.heading)
+          ? renderDiffHeading({ heading: diff.heading })
+          : null ),
+      groups
+        .map(function(group) {
+          var renderer = (
+            ( group.type === 'series' )
+              ? renderDiffSeries
+              : renderDiffParagraph )
+          return renderer({ data: group }) }) ])) }
--- test/browser.test.js
+++ test/browser.test.js
@@ -221,7 +221,35 @@ tape('Browser', function(test) {
         .then(function(existing) {
           test.assert(
             existing,
-            'Displays "whether" as a deletion.') }) }) })
+            'Displays "whether" as a deletion.') }) })
+
+    var abc = (
+      'b34c42c6b0620405d352d1987ab6b758' +
+      '6634c136c8b7fca26e15abc2ce3e62f0' )
+
+    var bcd = (
+      'ca4b4d97de8c014df46d17d17e1de72e' +
+      '8fb942e1b6f0d72b5eddf56af4418967' )
+
+    test.test(function(test) {
+      test.plan(1)
+      webdriver
+        .url('http://localhost:8000/forms/' + abc + '#compare:' + bcd)
+        .isExisting('//del/p[contains(text(),"This is A.")]')
+        .then(function(existing) {
+          test.assert(
+            existing,
+            'Displays This is A." as a deletion.') }) })
+
+    test.test(function(test) {
+      test.plan(1)
+      webdriver
+        .url('http://localhost:8000/forms/' + abc + '#compare:' + bcd)
+        .isExisting('//ins/p[contains(text(),"This is D.")]')
+        .then(function(existing) {
+          test.assert(
+            existing,
+            'Displays "This is D." as an insertion.') }) }) })
 
 tape.onFinish(function() {
   webdriver.end() })
