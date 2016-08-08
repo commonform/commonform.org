c12dbf4d61e36e1e71ac624c47b58c440c25cfac
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-08 16:41:39 -0700

    Show mode buttons in not-found view

--- views/not-found.js
+++ views/not-found.js
@@ -1,9 +1,11 @@
 var html = require('yo-yo')
+var modeButtons = require('./mode-buttons')
 
-module.exports = function () {
+module.exports = function (send) {
   return html`
     <div class=container>
       <article class=commonform>
+        ${modeButtons(null, send)}
         <p>Not found.</p>
       </article>
     </div>
