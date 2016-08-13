ddc5c7c66833d86d3cff12c1901eca18ce00f15f
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-12 17:59:38 -0700

    Update tests

--- test/edit.test.js
+++ test/edit.test.js
@@ -1,19 +1,32 @@
 var tape = require('tape')
 var webdriver = require('./webdriver')
 
+var TEST = (
+  '7e1da4b2412325c4aabc87a235b993e2' +
+  'e1c4e0c6a8e616d7965c6a126cdbdcfc'
+)
+var testURL = 'http://localhost:8000/forms/' + TEST
+
 tape('Edits', function (suite) {
   suite.test('change heading', function (test) {
     test.plan(1)
     var firstHeading = '(//input[@class="heading"])[1]'
     var newRoot = (
-      '396a923d7d702869d0265f56874231da' +
-      '218c8c9d2d8496ae2b0e54bf64ec815e'
+      'f7287d02fb9d25b8de9db61f3c065c96' +
+      '59a87fac298dc2e7c6ee484b18814f22'
     )
     webdriver
-    .url('http://localhost:8000')
+    .url(testURL)
     .waitForExist(firstHeading)
+    // Change to edit mode.
+    .click('//a[contains(@class, "edit")]')
+    // Select the only heading.
     .click(firstHeading)
-    .keys(' Back!')
+    // Backspace the current heading.
+    .keys('\uE003'.repeat(20))
+    // Type a new one.
+    .keys('Edited Heading')
+    // Hit Enter.
     .keys('\uE006')
     .waitForText('.digest', newRoot)
     .getUrl()
