ee4369fb85006b13bed97c0dd811eacd6c9def45
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-12 17:59:32 -0700

    Fix compare mode

--- views/read.js
+++ views/read.js
@@ -1,11 +1,5 @@
-var comparison = require('./comparison')
 var editor = require('./editor')
-var footer = require('./footer')
-var header = require('./header')
-var html = require('yo-yo')
 var loading = require('./loading')
-var menu = require('./menu')
-var modeButtons = require('./mode-buttons')
 
 module.exports = function read (digest, state, send) {
   var haveData = state.merkle && state.merkle.digest === digest
@@ -14,28 +8,6 @@ module.exports = function read (digest, state, send) {
       send('form:fetch', {digest: digest})
     })
   } else {
-    if (state.diff) {
-      return html`
-        <div class=container>
-          <article class=commonform>
-            ${modeButtons(state.mode, send)}
-            ${menu(state, send)}
-            ${
-              header(
-                state.merkle.digest,
-                state.publications,
-                state.comparing.merkle.digest,
-                state.comparing.publications,
-                send
-              )
-            }
-            ${comparison(state.diff, send)}
-            ${footer()}
-          </article>
-        </div>
-      `
-    } else {
-      return editor(state, send)
-    }
+    return editor(state, send)
   }
 }
