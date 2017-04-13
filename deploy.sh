93209b2ed87b61b6d9514b3f50fc578b2ad2a1df
Kyle E. Mitchell <kyle@kemitchell.com>
2017-04-12 17:41:18 -0700

    Improve deployment script

--- deploy.sh
+++ deploy.sh
@@ -19,21 +19,21 @@ REF=${1:-HEAD}
 	set -e
 
 	# The Git commit hash. This becomes the name of a new subdirectory in ROOT.
-	COMMIT=`git rev-parse "$REF"`
-	GITDIR=`pwd`
+  COMMIT=$(git rev-parse "$REF")
+  GITDIR=$(pwd)
 
 	# Make a temporary directory. The build script will do a clean-slate
 	# build there.
-	TMPDIR=`mktemp -d`
+  TMPDIR=$(mktemp -d)
 
 	# Remove the temporary directory if something goes wrong.
-	trap "{ cd "$GITDIR" ; rm -rf "$TMPDIR"; exit 255; }" SIGINT EXIT
+	trap '{ cd $GITDIR ; rm -rf $TMPDIR; exit 255; }' SIGINT EXIT
 
 	# Clone Git-tracked sources to the temporary directory.
 	git clone -s "$GITDIR" "$TMPDIR"
 
 	# Change to the temporary directory.
-	cd $TMPDIR
+	cd "$TMPDIR"
 
 	# Install dependencies.
 	npm install
@@ -52,7 +52,7 @@ REF=${1:-HEAD}
 	sed -ie "s/RELEASE\//https:\/\/commonform.org\/releases\/$COMMIT\//" $BUILD/index.html
 
 	# Copy build files to the new directory in ROOT.
-	rsync $FLAGS $BUILD/* commonform.org:$ROOT/releases/$COMMIT
+	rsync $FLAGS $BUILD/* "commonform.org:$ROOT/releases/$COMMIT"
 
 	# Copy the new index.html to ROOT itself, as well, replacing the
 	# current index.html. This ensures that new visitors will download
