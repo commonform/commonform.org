a7ab6a2898bc283d56c0a0364ad5e31a4089e53f
Kyle E. Mitchell <kyle@kemitchell.com>
2017-04-27 16:17:18 -0700

    Fix test shell script process management

--- test.sh
+++ test.sh
@@ -2,19 +2,30 @@
 set -e
 
 echo "Starting chromedriver."
-npm run chromedriver &> chromedriver.log &
+npm run chromedriver >chromedriver.log 2>&1 &
+CHROME_PID="$!"
 
 echo "Starting application and API servers."
-npm run start &> servers.log &
+npm run start >servers.log 2>&1 &
+START_PID="$!"
+
+function cleanup {
+  rkill -2 "$CHROME_PID" >/dev/null
+  rkill -2 "$START_PID" >/dev/null
+}
+
+trap cleanup EXIT
 
 echo "Building."
+
 npm run build:css &
 CSS_PID="$!"
+
 npm run build:js &
 JS_PID="$!"
+
 wait "$CSS_PID" "$JS_PID"
 
 echo "Running tests."
-npm test
 
-exit
+npm test
