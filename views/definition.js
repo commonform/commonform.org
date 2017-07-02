8ea3674c755e27b26058eec69dd586b0d75d5c1a
Kyle E. Mitchell <kyle@kemitchell.com>
2017-07-01 20:08:40 -0700

    Fix typo

--- views/definition.js
+++ views/definition.js
@@ -3,7 +3,7 @@ var assert = require('assert')
 module.exports = function definition (term) {
   assert(typeof term === 'string')
   var dfn = document.createElement('dfn')
-  dfn.setAttribute('titie', 'Definition of ' + term)
+  dfn.setAttribute('title', 'Definition of ' + term)
   dfn.id = 'Definition:' + term
   dfn.appendChild(document.createTextNode(term))
   return dfn
