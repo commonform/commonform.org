f9754a9e2a75f682661f5da9cea51d9ab6fbd6ba
Kyle E. Mitchell <kyle@kemitchell.com>
2018-05-29 16:28:48 -0700

    Form: Shorten to "Contents"

--- routes/partials/form.js
+++ routes/partials/form.js
@@ -28,7 +28,7 @@ module.exports = function (form, loaded, options) {
 function renderTableOfContents (form) {
   if (!containsHeading(form)) return ''
   return html`<header class=toc>
-    <h2>Table of Contents</h2>
+    <h2>Contents</h2>
     ${renderContents(form)}
   </header>`
 }
