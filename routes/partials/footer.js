adf5fadbec677e4e50b0a4a8a7caf6eb400afd9b
Kyle E. Mitchell <kyle@kemitchell.com>
2018-05-29 00:07:09 -0700

    Footer: Add target=_blank

--- routes/partials/footer.js
+++ routes/partials/footer.js
@@ -2,13 +2,16 @@ module.exports = function (/* scripts */) {
   return `
 <footer role=contentinfo>
   <p>
-    <a href=https://typographyforlawyers.com/equity.html>Equity</a>
+    <a href=https://typographyforlawyers.com/equity.html target=_blank>Equity</a>
     and
-    <a href=https://typographyforlawyers.com/triplicate.html>Triplicate</a>
+    <a href=https://typographyforlawyers.com/triplicate.html target=_blank>Triplicate</a>
     typefaces by
-    <a href=https://typographyforlawyers.com/about.html>Matthew Butterick</a>.
+    <a href=https://typographyforlawyers.com/about.html target=_blank>Matthew Butterick</a>.
   </p>
-  <p>Common Form is <a href=https://github.com/commonform>open source software</a>.</p>
+  <p>
+    Common Form is
+    <a href=https://github.com/commonform target=_blank>open source software</a>.
+  </p>
 </footer>
 ${
   Array.prototype.slice.call(arguments)
