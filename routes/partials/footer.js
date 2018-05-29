d2df5be271b0337216a5103002f7122b53efbe50
Kyle E. Mitchell <kyle@kemitchell.com>
2018-05-29 00:06:00 -0700

    Credit Matthew Butterick in footer

--- routes/partials/footer.js
+++ routes/partials/footer.js
@@ -1,7 +1,14 @@
 module.exports = function (/* scripts */) {
   return `
 <footer role=contentinfo>
-<p>Common Form is <a href=https://github.com/commonform>open source software</a>.</p>
+  <p>
+    <a href=https://typographyforlawyers.com/equity.html>Equity</a>
+    and
+    <a href=https://typographyforlawyers.com/triplicate.html>Triplicate</a>
+    typefaces by
+    <a href=https://typographyforlawyers.com/about.html>Matthew Butterick</a>.
+  </p>
+  <p>Common Form is <a href=https://github.com/commonform>open source software</a>.</p>
 </footer>
 ${
   Array.prototype.slice.call(arguments)
