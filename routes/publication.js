9fce1ff87d293d56ca08b632f2999db47c6927cb
Kyle E. Mitchell <kyle@kemitchell.com>
2018-05-31 07:31:42 -0700

    Remove completed TODO

--- routes/publication.js
+++ routes/publication.js
@@ -20,8 +20,6 @@ var preamble = require('./partials/preamble')
 var projectLink = require('./partials/project-link')
 var publisherLink = require('./partials/publisher-link')
 
-// TODO: Show release notes.
-
 module.exports = function (configuration, request, response) {
   if (request.method !== 'GET') {
     return methodNotAllowed.apply(null, arguments)
