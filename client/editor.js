2ac9cdce6c9a35f0e573c5db1fdd950fe9ce4fe9
Kyle E. Mitchell <kyle@kemitchell.com>
2019-06-16 17:43:03 -0700

    Editor: Refactor

--- client/editor.js
+++ client/editor.js
@@ -6,11 +6,15 @@ window.addEventListener('beforeunload', function (event) {
 })
 
 document.addEventListener('DOMContentLoaded', function () {
-  // If the user changes the content in the editor, mark it
-  // dirty, so we can warn on `beforeunload`.
+  setDirtyFlagOnEdit()
+})
+
+// If the user changes the content in the editor, mark it
+// dirty, so we can warn on `beforeunload`.
+function setDirtyFlagOnEdit () {
   var editors = [ 'editor', 'notes', 'signaturePages' ]
   editors.forEach(function (id) {
     var editor = document.getElementById(id)
     editor.addEventListener('input', function () { dirty = true })
   })
-})
+}
